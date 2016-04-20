3de6ad0973017a8ecbb0bb3374edbe191928c702
Shyam Ravikumar <shyam.ravikumar@gmail.com>
2016-04-20 13:42:56 +0530

    Changed gyro.h to support MPU.. initial tests - Works

--- Arduino/Examples/Gyro-mpu9250/Gyro-mpu9250.ino
+++ Arduino/Examples/Gyro-mpu9250/Gyro-mpu9250.ino
@@ -1,5 +1,8 @@
 #include <Mpu.h>
+<<<<<<< refs/remotes/origin/develop
 unsigned long timer;
+=======
+>>>>>>> Changed gyro.h to support MPU.. initial tests - Works
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -10,16 +13,23 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+<<<<<<< refs/remotes/origin/develop
   timer = millis();
 float *ypr = getYPR();
  timer = millis() - timer;
+=======
+float *ypr = getYPR();
+>>>>>>> Changed gyro.h to support MPU.. initial tests - Works
  Serial.print("ypr\t");
  Serial.print(ypr[0]);
  Serial.print("\t");
  Serial.print(ypr[1]);
  Serial.print("\t");
  Serial.print(ypr[2]);
+<<<<<<< refs/remotes/origin/develop
  Serial.print("\t");
  Serial.println(timer);
  
+=======
+>>>>>>> Changed gyro.h to support MPU.. initial tests - Works
 }
